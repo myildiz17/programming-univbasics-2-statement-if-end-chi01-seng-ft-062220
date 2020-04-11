7c3295e0cb4c059d2f4580a6d91658e50628c7ac
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-04-11 18:30:33 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,9 @@
-# Write your solution here
+current_time = Time.now
+puts "Number of seconds from 1-1-1970 until the time we run the program is:"
+number_of_seconds = current_time.to_i
+puts number_of_seconds
+if number_of_seconds % 2 == 0 
+  puts "Since it is divisible by 2, It is even"
+else
+  puts "Since it is not divisible by 2, it is odd"
+end
