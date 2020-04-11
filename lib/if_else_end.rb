59409aa1fcfbb8e8ba2426409fe09b34b2c84efb
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-04-11 18:20:28 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,5 +1,5 @@
 year = Time.now
-puts "Number of seconds from 1-1-1970 until the current second when run the program is:"
+puts "Number of seconds from 1-1-1970 until the time when we run the program is:"
 number_of_seconds = year.to_i
 puts number_of_seconds
 if number_of_seconds % 2 == 0 
