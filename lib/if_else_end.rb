7e3a18fbe3294df67876fa4ac1bebebc6ad922c5
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-04-11 18:24:18 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,6 +1,6 @@
-year = Time.now
+current_time = Time.now
 puts "Number of seconds from 1-1-1970 until the time we run the program is:"
-number_of_seconds = year.to_i
+number_of_seconds = current_time.to_i
 puts number_of_seconds
 if number_of_seconds % 2 == 0 
   puts "Since it is divisible by 2, It is even"
